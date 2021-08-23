26263a5209dc6e7727ec3ed08fbcb7b35b1f9974
wsingleton <wezley.singleton@gmail.com>
2021-08-23 17:56:02 -0400

    adding some challenges; solutions pushed to personal branch by 27AUG2021

---
+++ some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
@@ -0,0 +1,22 @@
+package com.revature.challenges.problem_4;
+
+/*
+    Problem Description:
+
+        A palindromic number reads the same both ways. The largest palindrome made from the product of
+        two 2-digit numbers is 9009 = 91 × 99.
+
+        Find the largest palindrome made from the product of two numbers whose digit length is "n".
+
+    Constraints:
+
+        1 <= n <= 4
+
+ */
+public class ProblemFour {
+
+    public int largestPalindromicNumber(int factorDigitLength) {
+        return -1;
+    }
+
+}
---
+++ some-challenges/src/test/java/com/revature/challenges/problem_1/ProblemOneTestSuite.java
@@ -0,0 +1,40 @@
+package com.revature.challenges.problem_1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    Problem Description:
+
+        If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+        The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+ */
+public class ProblemOneTestSuite {
+
+    ProblemOne sut;
+
+    @BeforeEach
+    public void setup() {
+        sut = new ProblemOne();
+    }
+
+    @Test
+    public void returns_23_given_10() {
+        assertEquals(23, sut.sumOfMultiples_3or5(10));
+    }
+
+    @Test
+    public void returns_233168_given_1000() {
+        assertEquals(233168, sut.sumOfMultiples_3or5(1000));
+    }
+
+    @Test
+    public void returns_23331668_given_10000() {
+        assertEquals(23331668, sut.sumOfMultiples_3or5(10000));
+    }
+
+
+}
