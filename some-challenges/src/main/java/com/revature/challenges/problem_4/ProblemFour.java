b1ce53b80face3fe32901ec898e3794890f7974a
LunaHaines <nhaines95@gmail.com>
2021-08-24 19:15:17 -0600

    problem 4 solution

--- some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
+++ some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
@@ -16,7 +16,30 @@ package com.revature.challenges.problem_4;
 public class ProblemFour {
 
     public int largestPalindromicNumber(int factorDigitLength) {
-        return -1;
+        String potentialPalindrome = "";
+        int biggestPalindrome = 0;
+        int startNum = 9;
+        if (factorDigitLength == 2) { startNum = 99; }
+        if (factorDigitLength == 3) { startNum = 999; }
+        if (factorDigitLength == 4) { startNum = 9999; }
+
+        for (int i = startNum; i > 0; i--) {
+            for (int j = startNum; j > 0; j--) {
+                potentialPalindrome = Integer.toString(i*j);
+                if (i*j > biggestPalindrome && potentialPalindrome.equals(reverse(potentialPalindrome))) {
+                    biggestPalindrome = i*j;
+                }
+            }
+        }
+        return biggestPalindrome;
+    }
+
+    public String reverse(String forward) {
+        String result = "";
+        for (int i = 0; i < forward.length(); i++) {
+            result += forward.substring((forward.length()-1)-i,forward.length()-i);
+        }
+        return result;
     }
 
 }
