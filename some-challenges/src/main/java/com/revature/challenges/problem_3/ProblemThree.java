91ec6d4708f7e8b4bd408e2dd35b8fece30f4a29
LunaHaines <nhaines95@gmail.com>
2021-08-24 18:56:11 -0600

    totally haven't written isPrime in every lagnuage I've learned

--- some-challenges/src/main/java/com/revature/challenges/problem_3/ProblemThree.java
+++ some-challenges/src/main/java/com/revature/challenges/problem_3/ProblemThree.java
@@ -18,7 +18,39 @@ import java.util.List;
 public class ProblemThree {
 
     public List<Integer> findPrimeFactors(int candidate) {
-        return null;
+
+        List<Integer> result = new ArrayList<>();
+        boolean haveFactorization = false;
+        int toFactor = candidate;
+        int factor;
+
+        while (!haveFactorization) {
+            factor = smallestFactor(toFactor);
+            if (factor == 1) {
+                result.add(toFactor);
+                haveFactorization = true;
+            } else if (isPrime(factor)) {
+                result.add(factor);
+                toFactor /= factor;
+            }
+        }
+        return result;
+    }
+
+    public boolean isPrime(int potentialPrime) {
+        if (potentialPrime == 1) { return false; }
+        if (potentialPrime == 2) { return true; }
+        for (int i = 2; i < (potentialPrime/2)+1; i++) {
+            if (potentialPrime%i == 0) { return false; }
+        }
+        return true;
+    }
+
+    public int smallestFactor(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number%i == 0) { return i; }
+        }
+        return 1;
     }
 
 }
