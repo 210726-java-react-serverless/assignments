27e3661a0af56b165802fac929f5a621884a5065
hpuf <csmcdn@gmail.com>
2021-08-26 17:20:36 -0500

    done

--- some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
+++ some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
@@ -13,18 +13,51 @@ package com.revature.challenges.problem_4;
         1 <= n <= 4
 
  */
+
+// This could probably work with Long but god would it take a *while* to execute.
 public class ProblemFour {
 
     public int largestPalindromicNumber(int factorDigitLength) {
 
         double i = 1;
+        double o = 1;
         int a = 1;
+        int checkNum = 0;
+        int topNum = 0;
 
         while(Math.log10(i)<factorDigitLength) {
-
+            while(Math.log10(o)<factorDigitLength){
+                checkNum = (int) (o * i);
+                if (isPalindrome(checkNum))
+                    if (checkNum>topNum)
+                        topNum = checkNum;
+                o++;
+            }
+            i++;
+            o=i;
         }
 
-        return factorDigitLength;
+        return topNum;
+    }
+
+
+    public boolean isPalindrome(int num) {
+
+        int flipNumber = num;
+        int reversed = 0;
+        int remaining;
+
+        // This took a while because I wanted to do it mathematically rather than with a string lol
+        while(flipNumber != 0){
+            // Get the first digit
+            remaining = flipNumber % 10;
+            // Bump first digit up (if it exists) and then slot in the new digit
+            reversed = reversed * 10 + remaining;
+            // Drop the first number and repeat if there are numbers left.
+            flipNumber /= 10;
+        }
+
+        return num == reversed;
     }
 
 }
