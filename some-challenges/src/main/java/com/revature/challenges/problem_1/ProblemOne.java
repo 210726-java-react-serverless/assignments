906ab496ca10ec9490eb7bd6573fd435766fc6ea
kimatec <kimtranatec14@gmail.com>
2021-08-27 16:04:02 -0500

    fixing

--- some-challenges/src/main/java/com/revature/challenges/problem_1/ProblemOne.java
+++ some-challenges/src/main/java/com/revature/challenges/problem_1/ProblemOne.java
@@ -22,7 +22,7 @@ public class ProblemOne {
         int n = exclusiveEnd;
         int sum = 0;
 
-        for( int i = 1; i <= n; i++){
+        for( int i = 0; i < n; i++){
             if( i % 3 == 0 || i % 5 == 0)
                 sum += i;
         }
