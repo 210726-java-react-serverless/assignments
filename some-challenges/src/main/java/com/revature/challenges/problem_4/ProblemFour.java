c265f40aec233cf48e6d3c82f25fd921ecae48f5
Robert Ni <robertni@outlook.fr>
2021-08-27 14:28:10 -0400

    solved problem 4

--- some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
+++ some-challenges/src/main/java/com/revature/challenges/problem_4/ProblemFour.java
@@ -16,7 +16,30 @@ package com.revature.challenges.problem_4;
 public class ProblemFour {
 
     public int largestPalindromicNumber(int factorDigitLength) {
-        return -1;
+        int max = 0;
+        int upperLimit = (int) Math.pow(10, factorDigitLength);
+        int lowerLimit = (int) Math.pow(10, factorDigitLength - 1);
+
+        for (int i = lowerLimit; i < upperLimit; i++) {
+            for (int j = lowerLimit; j < upperLimit; j++) {
+                int temp = i * j;
+                if (temp == checkReverse(temp) && temp > max) {
+                    max = temp;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public int checkReverse(int num) {
+        int reverse = 0;
+
+        while (num > 0) {
+            reverse = reverse * 10 + num % 10;
+            num = num / 10;
+        }
+        return reverse;
     }
 
 }
