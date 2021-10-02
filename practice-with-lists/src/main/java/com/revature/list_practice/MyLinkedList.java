5b67594614ec431fb5eadd8503d59e90a5cf8816
LunaHaines <nhaines95@gmail.com>
2021-10-02 15:43:50 -0600

    null technically palindromic => return true

--- practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
+++ practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
@@ -212,7 +212,7 @@ public class MyLinkedList<T> {
      * @return true if the contents of this list form a palindrome
      */
     public boolean isPalindromicList() {
-        if (this == null || this.head == null) { return null; }
+        if (this == null || this.head == null) { return true; }
     }
 
     static class Node<T> {
