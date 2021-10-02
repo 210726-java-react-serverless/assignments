e67268f8a836c5311afab263c30e895cff91abbe
LunaHaines <nhaines95@gmail.com>
2021-10-02 15:12:38 -0600

    iterating is important

--- practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
+++ practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
@@ -69,7 +69,9 @@ public class MyLinkedList<T> {
         do {
             if (currentNode.nextNode == null) {
                 currentNode.nextNode = addNode;
+                continue;
             }
+            currentNode = currentNode.nextNode
         } while (!currentNode.nextNode.equals(addNode));
         return true;
     }
@@ -138,15 +140,15 @@ public class MyLinkedList<T> {
      * @return true if this list contains a loop
      */
     public boolean containsLoop() {
-//        if (this == null || this.head == null) { return false; }
-//        if (this.head.nextNode == null) { return false; }
-//        MyLinkedList<T> copy = new MyLinkedList<>();
-//        Node<T> currentNode = this.head;
-//        while (currentNode.nextNode != null) {
-//            if (copy.contains(currentNode.data)) { return true; }
-//            copy.add(currentNode.data);
-//            currentNode = currentNode.nextNode;
-//        }
+        if (this == null || this.head == null) { return false; }
+        if (this.head.nextNode == null) { return false; }
+        MyLinkedList<T> copy = new MyLinkedList<>();
+        Node<T> currentNode = this.head;
+        while (currentNode.nextNode != null) {
+            if (copy.contains(currentNode.data)) { return true; }
+            copy.add(currentNode.data);
+            currentNode = currentNode.nextNode;
+        }
         return false;
     }
 
