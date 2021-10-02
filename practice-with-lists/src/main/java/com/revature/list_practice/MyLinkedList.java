eb4166d2cf7bd062440b3caf2ac74837cce1a753
LunaHaines <nhaines95@gmail.com>
2021-10-02 15:27:49 -0600

    added size 1 case

--- practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
+++ practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
@@ -195,7 +195,11 @@ public class MyLinkedList<T> {
         int size = 0;
         Map<Integer, T> index = new HashMap<>();
         Node<T> currentNode = this.head;
-
+        if (currentNode.nextNode == null) {
+            if (k > 1) { return null; }
+            if (k == 0) { return currentNode.nextNode.data; }
+            if (k == 1) { return currentNode.data; }
+        }
     }
 
     /**
