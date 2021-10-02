4e9a82316ffc847efc2d271d034e8f4e6eba5ca9
Sean Dunn <60554213+spdunn@users.noreply.github.com>
2021-10-01 20:59:16 -0400

    Implemented peek
    Signed-off-by: Sean Dunn <60554213+spdunn@users.noreply.github.com>

--- practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
+++ practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
@@ -114,7 +114,8 @@ public class MyLinkedList<T> {
      * @return the head of this list, or null if this list is empty
      */
     public T peek() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        if (this.head == null) return null;
+        return this.head.data;
     }
 
     /**
