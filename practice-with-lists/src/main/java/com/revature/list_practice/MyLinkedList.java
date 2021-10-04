f8b14317ab1355721db1c0ec78de52eedb0dc905
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-10-04 02:36:27 -0400

    completed assignment

--- practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
+++ practice-with-lists/src/main/java/com/revature/list_practice/MyLinkedList.java
@@ -1,5 +1,12 @@
 package com.revature.list_practice;
 
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * A simple implementation of a singly linked list.
  *
@@ -23,7 +30,7 @@ public class MyLinkedList<T> {
      * @return true if this collection contains no elements
      */
     public boolean isEmpty() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        return this.head == null? true: false;
     }
 
     /**
@@ -35,7 +42,14 @@ public class MyLinkedList<T> {
      * @return true if this collection contains the specified element
      */
     public boolean contains(T t) {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        if (this.isEmpty())
+            return false;
+        if (this.head.data.equals(t))
+            return true;
+        else {
+            MyLinkedList<T> nextList = new MyLinkedList<>(this.head.nextNode);
+            return nextList.contains(t);
+        }
     }
 
     /**
@@ -48,7 +62,21 @@ public class MyLinkedList<T> {
      * @return true if this collection changed as a result of the call
      */
     public boolean add(T t) {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        if (t == null)
+            return false;
+        if (this.contains(t))
+            return false;
+        if (this.head == null){
+            this.head = new MyLinkedList.Node<>(t);
+            this.head.nextNode = null;
+            return true;
+        }
+        MyLinkedList.Node<T> node = this.head;
+        while (node.nextNode != null){
+            node = node.nextNode;
+        }
+        node.nextNode = new MyLinkedList.Node<>(t);
+        return true;
     }
 
     /**
@@ -62,7 +90,21 @@ public class MyLinkedList<T> {
      * @return true if this list contained the specified element
      */
     public boolean remove(Object o) {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        if (this.isEmpty())
+            return false;
+        if (this.head.nextNode == null)
+            return false;
+        if (this.head.nextNode.data.equals(o)) {
+            this.head.nextNode = this.head.nextNode.nextNode;
+            return true;
+        }
+        if (this.head.data.equals(o)) {
+            this.head = this.head.nextNode;
+            return true;
+        }else {
+            this.head = this.head.nextNode;
+            this.remove(o);
+        } return false;
     }
 
     /**
@@ -71,7 +113,11 @@ public class MyLinkedList<T> {
      * @return the head of this list, or null if this list is empty
      */
     public T poll() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        if(this.isEmpty())
+            return null;
+        MyLinkedList.Node<T> firstNode = new MyLinkedList.Node<T>(this.head.data);
+        this.remove(this.head.data);
+        return firstNode.data;
     }
 
     /**
@@ -80,7 +126,9 @@ public class MyLinkedList<T> {
      * @return the head of this list, or null if this list is empty
      */
     public T peek() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        if (this.isEmpty())
+            return null;
+        return this.head.data;
     }
 
     /**
@@ -94,7 +142,16 @@ public class MyLinkedList<T> {
      * @return true if this list contains a loop
      */
     public boolean containsLoop() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        MyLinkedList<T> nodeList = new MyLinkedList<>();
+        MyLinkedList<T> listCopy = new MyLinkedList<>(this.head);
+        if (this.isEmpty())
+            return false;
+        while (!nodeList.contains(listCopy.head.data)){
+            if(listCopy.head.nextNode == null)
+                return false;
+            nodeList.add(listCopy.head.data);
+            listCopy.head = listCopy.head.nextNode;
+        } return true;
     }
 
     /**
@@ -106,7 +163,12 @@ public class MyLinkedList<T> {
      *
      */
     public MyLinkedList<T> removeDuplicates() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        MyLinkedList<T> noDuplicatesList = new MyLinkedList<>();
+        MyLinkedList.Node<T> node = this.head;
+        while (!(noDuplicatesList.contains(node.data)) && node.nextNode != null) {
+            noDuplicatesList.add(node.data);
+            node = node.nextNode;
+        } return noDuplicatesList;
     }
 
     /**
@@ -120,7 +182,29 @@ public class MyLinkedList<T> {
      * @return the k-th to last element of this list
      */
     public T getKthToLast(int k) {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        MyLinkedList.Node<T> lastNode = this.head;
+        MyLinkedList.Node<T> lastIter = null;
+        MyLinkedList<T> reversed = new MyLinkedList<>();
+        int size = 0;
+        while (lastNode != null) {
+            lastNode = lastNode.nextNode;
+            size += 1;
+        }
+        if (k>size || k<0){
+            return null;
+        }
+        for (int i=0; i<size; i++){
+            MyLinkedList.Node<T> iterator = this.head;
+            while (iterator.nextNode != lastIter){
+                iterator = iterator.nextNode;
+            }
+            reversed.add(iterator.data);
+            lastIter = iterator;
+        }
+        for (int j=0; j<k; j++){
+            reversed.head= reversed.head.nextNode;
+        }
+        return reversed.head.data;
     }
 
     /**
@@ -129,14 +213,24 @@ public class MyLinkedList<T> {
      * @return true if the contents of this list form a palindrome
      */
     public boolean isPalindromicList() {
-        throw new ImplementationMissingException(); // TODO: REPLACE THIS
+        MyLinkedList<T> copy = new MyLinkedList<>(this.head);
+        int i = 0;
+        while (copy.peek() != null) {
+            try {
+                if (!(copy.poll().equals(this.getKthToLast(i))))
+                    return false;
+                i++;
+            } catch (NullPointerException npe) {
+                return true;
+            }
+        } return true;
     }
 
-    static class Node<T> {
-        T data;
-        Node<T> nextNode;
+    public static class Node<T> {
+        public T data;
+        public Node<T> nextNode;
 
-        Node(T data) {
+        public Node(T data) {
             this.data = data;
         }
     }
